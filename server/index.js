898cdd27b558aaabbee84860a8aaf3fb72d0fafe
Sumit <sumit1711kd@gmail.com>
2025-01-19 21:51:15 +0530

    Second Commit

--- server/index.js
+++ server/index.js
@@ -17,12 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req,res)=>{
-    res.send("Hello");
-})
-
 app.use("/api", emailRouter);
-
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 app.get("*", (req,res)=>{
